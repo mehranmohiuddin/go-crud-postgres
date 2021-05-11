e81c9f415f216d8bf7805280b7785089394381cd
mehranmohiuddin <mohiuddin.mehran@gmail.com>
2021-05-10 22:22:33 -0400

    Separating out routes and models packages

--- main.go
+++ main.go
@@ -1,67 +1,16 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 
-	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
+	"github.com/mehranmohiuddin/go-crud-postgres/router"
 )
-
-type User struct {
-	ID       int
-	Name     string
-	Age      int
-	Location string
-}
 
 func main() {
 	fmt.Println("Running on port 8080")
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "hello world")
-	}).Methods("GET")
-
-	r.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Error oading .env file")
-		}
-
-		db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
-		if err != nil {
-			log.Fatal("failed to open a db connection:", err)
-		}
-		defer db.Close()
-
-		rows, err := db.Query("SELECT * FROM users")
-		if err != nil {
-			log.Fatal("Failed to execute query: ", err)
-		}
-		defer rows.Close()
-
-		var users []User
-		for rows.Next() {
-			var user User
-
-			err = rows.Scan(&user.ID, &user.Name, &user.Age, &user.Location)
-			if err != nil {
-				log.Fatalf("Unable to scan the row. %v", err)
-			}
-
-			users = append(users, user)
-		}
-
-		json.NewEncoder(w).Encode(users)
-	}).Methods("GET")
+	r := router.Router()
 
 	http.ListenAndServe(":8080", r)
 }
